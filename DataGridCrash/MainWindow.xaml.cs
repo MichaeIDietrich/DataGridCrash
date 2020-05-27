476000ae247bb10cea49f8150fb71f92a488c95f
Michael Dietrich <dev.michaeldietrich@outlook.de>
2020-05-27 12:01:31 +0200

    Add code to reproduce crash in DataGrid

---
+++ DataGridCrash/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace DataGridCrash
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataGrid.ItemsSource = new ObservableCollection<DataGridItem>();
+
+            KeyUp += OnKeyUp;
+        }
+        
+        private void OnKeyUp( object sender, KeyEventArgs e )
+        {
+            // Pressing F1 is meant to show an overlay and disable the current content
+            if( e.Key == Key.F1 )
+                DataGrid.IsEnabled = false;
+        }
+        
+        class DataGridItem
+        {
+            public string Text { get; set; } 
+        }
+    }
+}
